0d97e5ffcbfc19a28440b818be29bbfc7bdc772a
DomCR <DomCR@users.noreply.github.com>
2024-09-04 18:58:20 +0200

    tests

---
+++ src/ACadSharp.Pdf.Tests/PdfExporterTests.cs
@@ -0,0 +1,33 @@
+using ACadSharp.IO;
+using System.IO;
+using Xunit;
+
+namespace ACadSharp.Pdf.Tests
+{
+	public class PdfExporterTests
+	{
+		private CadDocument _document;
+
+		[Fact]
+		public void AddModelSpaceTest()
+		{
+			string filename = Path.Combine(TestVariables.OutputSamplesFolder, "model.pdf");
+			using (PdfExporter exporter = new PdfExporter(filename))
+			{
+				exporter.Configuration.ReferenceDocument = this.getDocument();
+				exporter.AddModelSpace();
+
+				exporter.Close();
+			}
+		}
+
+		private CadDocument getDocument()
+		{
+			if (this._document == null)
+			{
+				this._document = DwgReader.Read(Path.Combine(TestVariables.SamplesFolder, "export_sample.dwg"));
+			}
+			return this._document;
+		}
+	}
+}
---
+++ src/ACadSharp.Pdf.Tests/TestVariables.cs
@@ -0,0 +1,37 @@
+﻿using CSUtilities;
+using System;
+using System.IO;
+
+namespace ACadSharp.Pdf.Tests
+{
+	public static class TestVariables
+	{
+		public static string DesktopFolder { get { return Environment.GetFolderPath(Environment.SpecialFolder.Desktop); } }
+
+		public static string SamplesFolder { get { return EnvironmentVars.Get<string>("SAMPLES_FOLDER"); } }
+
+		public static string OutputSamplesFolder { get { return EnvironmentVars.Get<string>("OUTPUT_SAMPLES_FOLDER"); } }
+
+		public static bool LocalEnv { get { return EnvironmentVars.Get<bool>("LOCAL_ENV"); } }
+
+		static TestVariables()
+		{
+			EnvironmentVars.SetIfNull("SAMPLES_FOLDER", "../../../../../samples/");
+			EnvironmentVars.SetIfNull("OUTPUT_SAMPLES_FOLDER", "../../../../../samples/out");
+			EnvironmentVars.SetIfNull("LOCAL_ENV", "true");
+		}
+
+		public static void CreateOutputFolders()
+		{
+			craateFolderIfDoesNotExist(OutputSamplesFolder);
+		}
+
+		private static void craateFolderIfDoesNotExist(string path)
+		{
+			if (!Directory.Exists(path))
+			{
+				Directory.CreateDirectory(path);
+			}
+		}
+	}
+}
