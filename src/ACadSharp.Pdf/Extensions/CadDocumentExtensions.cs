34635fd5e7b5e56c59742a32cb312c07f623584c
DomCR <DomCR@users.noreply.github.com>
2025-04-06 22:48:42 +0200

    craete preview

---
+++ src/ACadSharp.Pdf/Extensions/CadDocumentExtensions.cs
@@ -0,0 +1,30 @@
+﻿using ACadSharp.IO;
+using Svg;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace ACadSharp.Pdf.Extensions
+{
+	public static class CadDocumentExtensions
+	{
+		public static DwgPreview CreatePreview(this CadDocument document)
+		{
+			MemoryStream svgStream = new MemoryStream();
+			using (SvgWriter writer = new SvgWriter(svgStream, document))
+			{
+				writer.Write();
+			}
+
+			var svg = SvgDocument.Open<SvgDocument>(new MemoryStream(svgStream.GetBuffer()));
+			svg.Transforms.Clear();
+
+			var bitmap = svg.Draw();
+			bitmap.RotateFlip(System.Drawing.RotateFlipType.Rotate180FlipX);
+
+			MemoryStream imageStream = new MemoryStream();
+			bitmap.Save(imageStream, ImageFormat.Png);
+
+			return new DwgPreview(DwgPreview.PreviewType.Png, new byte[80], imageStream.GetBuffer());
+		}
+	}
+}
